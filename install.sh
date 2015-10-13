924e1656eddd20368b257dfb389e092412f8096b
Yan Bai <baiyan718@gmail.com>
2015-10-13 20:53:32 +0000

    update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,18 @@
-sudo -s
-apt-get install python3
-apt-get install python3-lxml
-apt-get install csh
-apt-get install build-essential
-cpan
->install Algorithm::Munkres
+sudo apt-get update
+echo "===================================="
+echo "==========install python 3=========="
+echo "===================================="
+sudo apt-get install python3
+echo "===================================="
+echo "==========install lxml=============="
+echo "===================================="
+sudo apt-get install python3-lxml
+echo "===================================="
+echo "============install csh============="
+echo "===================================="
+sudo apt-get install csh
+echo "===================================="
+echo "============install make============"
+echo "===================================="
+sudo apt-get install build-essential
+sudo cpan > install Algorithm::Munkres
