5c6f8b12e335f7272b8a3c0f54a10f1719a801cd
Ubuntu <ubuntu@ip-172-31-38-177.us-west-2.compute.internal>
2015-10-13 21:55:45 +0000

    added comment in install.sh and runit.sh.

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
+#This script install necessary packages need for the running of our program.
+#
+#1.Python 3.
+#2.lxml. It is a external library for parsing XML file. It is stronger than Python built-in XML parser.
+#3.csh. Since we need to call shell command to run Sensecluster_scorer perl script, so we use csh instead of bash.
+#4.builder-essential. We need it to compile Algorithm-Munkres.
+#5.Algorithm-Munkres. Module for Sensecluster_scorer
+
 sudo apt-get update
 echo "===================================="
 echo "==========install python 3=========="
--- runit.sh
+++ runit.sh
@@ -1,8 +1,9 @@
 #! /bin/sh
 #
-#this script receive input data file path, 1 or 2 target words.
-#then start Main.py.
-
+#This script receives input data file path, 1 or 2 target words. Then start Main.py.
+#
+#Example 1: ./runit.sh teamdata/commit.xml commit
+#Example 2: ./runit.sh teamdata/government_year.xml government year
 
 if [ $# = 2 ]   #if 2 arguments found, pass input file and target words to Main.py
 then
