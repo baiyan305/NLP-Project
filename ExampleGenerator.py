c352a75db5f176e06ac6ef8a24919249d7cf22ca
naidu018 <naidu018@d.umn.edu>
2015-12-11 09:44:13 -0600

    frequencycount addition

--- ExampleGenerator.py
+++ ExampleGenerator.py
@@ -5,7 +5,32 @@ class ExampleGenerator(object):
 
 	def sentences_intersection(self,s1,s2):
 		return len(set(s1).intersection(s2))/((len(s1)+len(s2))/2)
+	
+	def get_length(content):
+		length=0
+		for listitem in content:
+			length=length+len(listitem)
+		return length
 
+	def collect_topwords(self,content):
+		mostcommonwords=[]
+		for items in content:
+			word_counter={}
+			for word in items:
+				if word in word_counter:
+					word_counter[word]+=1
+				else:
+					word_counter[word]=1
+			popular_words=sorted(word_counter,key=word_counter.get,reverse=True)
+			normalizedcount=self.get_length(content)/10
+			topn=popular_words[:normalizedcount]
+			mostcommonwords.append(topn)
+		return mostcommonwords
+	
+	def get_frequency_score(self,content,i):
+		thematicwords=self.collect_topwords(content)
+		return len(set(content[i]).intersection(thematicwords))/((len(content[i])+len(thematicwords))/2)
+		
 	def get_sentence_ranks(self,content,temporary):
 		#print(content[6])
 		n=len(content)
@@ -13,7 +38,6 @@ class ExampleGenerator(object):
 		global inkjuif
 		#print(n)
 		values=[[0 for x in xrange(n)] for x in xrange(n)]
-
 		for i in range(0,n):
 			score=0
 			for j in range(0,n):
@@ -24,11 +48,11 @@ class ExampleGenerator(object):
 					score+=values[i][j]				
 				else:			
 					values[i][j]=self.sentences_intersection(content[i],content[j])
-					score+=values[i][j]			
+					score+=values[i][j]
+			score+=self.get_frequence_score(content,i)			
 			if(score>=max):
 				max=score
 				inkjuif=i
-                
 		return temporary[inkjuif]
 
 	def get_examples(self, clusters,instances):
